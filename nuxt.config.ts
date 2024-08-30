856382c75bb67c7c56793be5911b736d423cf95b
qiaozhididi <1343013603@qq.com>
2024-08-30 22:42:48 +0800

    页面数据渲染

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  app: {
+    head: {
+      script: [
+        // <script src="https://myawesome-lib.js"></script>
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js",
+        },
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.5/js/swiper.min.js",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.5/css/swiper.min.css",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css",
+        },
+      ],
+    },
+  },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
-    <div>
-      <h1>欢迎来到首页</h1>
-      <AppAlert>
-        这是一个自动导入的组件。
-      </AppAlert>
+  <div class="blog-slider">
+    <div class="blog-slider__wrp swiper-wrapper">
+      <div
+        class="blog-slider__item swiper-slide"
+        v-for="(data, index) in dataJson"
+        ::key="index"
+      >
+        <div class="blog-slider__img">
+          <img :src="data?.image" alt="" />
+        </div>
+        <div class="blog-slider__content">
+          <div class="blog-slider__title">
+            <i :class="data?.icon"></i
+            ><span class="text">{{ data?.title }}</span>
+          </div>
+          <div class="blog-slider__text">
+            {{ data?.text }}
+          </div>
+          <a :href="data?.href" class="blog-slider__button">点击跳转</a>
+        </div>
+      </div>
     </div>
-  </template>
-  
+  </div>
+</template>
+
+<script lang="ts" setup>
+import dataJson from "../public/data/data.json";
+</script>
+<style scoped>
+
+</style>
