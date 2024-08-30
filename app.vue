07b70778b1239e9257e2884726287c09c342611d
qiaozhididi <1343013603@qq.com>
2024-08-30 23:25:25 +0800

    引入swiper

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
 <template>
   <NuxtPage />
 </template>
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  width: 100%;
+  background-image: linear-gradient(147deg, #bc59c6 0%, #7dc4cc 74%);
+  min-height: 100vh;
+  font-family: "Fira Sans", sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
+  modules: ["nuxt-swiper"],
+  swiper: {
+    // Swiper options
+    //----------------------
+    // prefix: 'Swiper',
+    // styleLang: 'css',
+    // modules: ['navigation', 'pagination'], // all modules are imported by default
+  },
   app: {
     head: {
       script: [
@@ -7,15 +15,8 @@ export default defineNuxtConfig({
         {
           src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js",
         },
-        {
-          src: "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.5/js/swiper.min.js",
-        },
       ],
       link: [
-        {
-          rel: "stylesheet",
-          href: "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.5/css/swiper.min.css",
-        },
         {
           rel: "stylesheet",
           href: "https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css",
